9e840b49d7f2e8078541d79ca8d0d010c80ed7d3
magicwubiao <magicwubiao@163.com>
2026-06-03 11:45:04 +0800

    chore: remove debug logging from buildToolsets

--- internal/server/server.go
+++ internal/server/server.go
@@ -1799,7 +1799,6 @@ func (s *Server) buildToolsets() []map[string]interface{} {
 	ungrouped := []string{}
 
 	allTools := s.toolReg.List()
-	fmt.Printf("[DEBUG] Total tools registered: %d, tools: %v\n", len(allTools), allTools)
 	for _, name := range allTools {
 		categorized := false
 		for prefix, catName := range categoryMap {
